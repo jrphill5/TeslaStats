bfbc04f4065feaf206c673270959388e5a92e870
Jay Phillips <rocketman8473@gmail.com>
2011-12-26 17:37:45 -0500

    updated centering function

--- center.c
+++ center.c
@@ -1,13 +1,14 @@
 void center( char* begin, char* text, int col, char pad, char* end )
 {
 
-	int i, j = 0.5 * ( col - strlen(text) - 2 );
+	int length = strlen(text);
+	int i, j = 0.5 * ( col - length - 2 );
 
 	printf("%s", begin);
 	for ( i = 0; i < j; i++ ) printf("%c",pad);
 	if ( *text != '\0' ) printf(" %s ", text);
 	else printf("%c%c", pad, pad);
-	j += 2 + strlen(text);
+	j += 2 + length;
 	for ( i = j; i < col; i++ ) printf("%c",pad);
 	printf("%s", end);
 
